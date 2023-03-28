2e3915b8b058afbe4e3c898d6465afe188d5d930
AlexandrStopkin <stnalex1@gmail.com>
2023-03-28 16:13:58 +0200

    Initial commit

---
+++ src/Person.java
@@ -0,0 +1,20 @@
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Objects;
+
+// Задача 1.
+// Создайте класс Person, которому при создании передавайте имя и дату рождения в виде строки
+// в формате "ДД.ММ.ГГГГ".
+//
+// Реализуйте в классе интерфейс Comparable<Person>, в котором "меньше" будет значить "младше".
+//
+// Подсказка: не обязательно вычислять возраст, этому мы так и не научились.
+// Но можно сравнивать даты рождения!
+//
+// Альтернативная подсказка: если очень хочется вычислять возраст,
+// то можно это делать в миллисекундах с использованием getTime()!
+public class Person implements Comparable<Person> {
+
+}
