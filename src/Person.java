63310d51f80740e650a33e0648f7b0b671687d15
AlexandrStopkin <stnalex1@gmail.com>
2023-03-28 16:15:10 +0200

    Add fields Constructor toString(), equals(), hashcode(), compareTo()

--- src/Person.java
+++ src/Person.java
@@ -16,5 +16,63 @@ import java.util.Objects;
 // Альтернативная подсказка: если очень хочется вычислять возраст,
 // то можно это делать в миллисекундах с использованием getTime()!
 public class Person implements Comparable<Person> {
+  private final String name;
+  private final Date born;
 
+  public String getName() {
+    return this.name;
+  }
+
+  public Date getBorn() {
+    return this.born;
+  }
+
+  public Person(String name, String birthday) throws ParseException {
+    DateFormat formatter = new SimpleDateFormat("dd.MM.yyyy");
+    if (name == null) {
+      throw new IllegalArgumentException("Name can't be null");
+    }
+    if (birthday == null) {
+      throw new IllegalArgumentException("birthday can't be null");
+    }
+    this.name = name;
+    this.born = formatter.parse(birthday);
+  }
+
+  @Override
+  public String toString() {
+    DateFormat formatter = new SimpleDateFormat("dd.MM.yyyy");
+    return "Person{" +
+        "name='" + name + '\'' +
+        ", born=" + formatter.format(born) +
+        '}';
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Person person = (Person) o;
+    return name.equals(person.name) && born.equals(person.born);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(name, born);
+  }
+
+  @Override
+  public int compareTo(Person o) {
+    if (born.before(o.born)) {
+      return 1;
+    } else if (o.born.before(born)) {
+      return -1;
+    } else {
+      return 0;
+    }
+  }
 }
